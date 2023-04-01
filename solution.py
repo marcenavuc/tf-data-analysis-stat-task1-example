38f98b1932766d956f7f58aba737a1bb4b2fbcdd
marcenavuc <mark.averchenko@gmail.com>
2023-04-01 13:56:50 +0500

    add likelihood

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 351730666 # Ваш chat ID, не меняйте название переменной
+
+
+def likelihood_func(mu: float, x: np.array) -> float:
+    n = len(x)
+    return -n * np.log(mu) - np.sum(x) / mu
+
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    res = minimize(likelihood_func, x0=1, args=(x,), method='Nelder-Mead')
+    return res.x[0]
