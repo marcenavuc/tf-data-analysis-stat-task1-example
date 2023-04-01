a27695bc6f06b7857a7e11daf5bbf20bcff3d401
marcenavuc <mark.averchenko@gmail.com>
2023-04-01 16:10:58 +0500

    att 2

--- solution.py
+++ solution.py
@@ -6,11 +6,5 @@ from scipy.optimize import minimize
 chat_id = 351730666 # Ваш chat ID, не меняйте название переменной
 
 
-def likelihood_func(mu: float, x: np.array) -> float:
-    n = len(x)
-    return -n * np.log(mu) - np.sum(x) / mu
-
-
 def solution(x: np.array) -> float:
-    res = minimize(likelihood_func, x0=1, args=(x,), method='Nelder-Mead')
-    return res.x[0]
+    return np.mean((x - (35 + np.e)) / 10)
